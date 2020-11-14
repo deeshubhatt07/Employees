1ea10706f0efd7c13a7e67d614d17f4a424e4ae4
deeshubhatt07 <deeshubhatt07@gmail.com>
2020-11-14 10:12:22 +0530

    Added icons

--- src/components/Postlists.js
+++ src/components/Postlists.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import '../components/Postlists.css';
 import PostData from '../data/PostData.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faPlus , faTrash } from '@fortawesome/free-solid-svg-icons';
 
 class Postlists extends Component{
     render(){
@@ -8,13 +10,15 @@ class Postlists extends Component{
             <div>
                 {PostData.map((Details, index) => {
                     return(
-                        <div>
-                            <div id="postitems">
-                                <h5>{Details.name}</h5>
+                        <div id="postitems">
+                            <div>
+                                <h5>{Details.name} (Position:{Details.depart})</h5>
                                 <p>Age: {Details.age} Salary:{Details.sal}</p>    
                             </div>
-                            <div>
-
+                            <div id="icon">
+                                <FontAwesomeIcon icon={faPlus}/> <span> </span>
+                                <FontAwesomeIcon icon={faEdit}/>  <span> </span>
+                                <FontAwesomeIcon icon={faTrash}/>
                             </div>    
                         </div>
                     )
